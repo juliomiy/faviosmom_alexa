ad6eb04f4a5f790446c224287c4ca65ff4799fe5
julio hernandez-miyares <jmiyares@usatoday.com>
2016-10-31 23:24:00 -0400

    work Monday Ocotober 31 2016

--- faviosmom_helper.js
+++ faviosmom_helper.js
@@ -4,12 +4,21 @@
 'use strict';
 module.change_code = 1;
 
+//var config = require('config').has('Faviosmom');
+var config = require('config');
+
 var _ = require('lodash');
 var rp = require('request-promise');
-var ENDPOINT = 'http://localhost:8000/v1/';
+var ENDPOINT = (config ? (config.has('apiConfig.endpoint')) ? config.get('apiConfig.endpoint') : "http://104.155.167.140:8000/v1/" : "http://104.155.167.140:8000/v1/") ;
+
+//var ENDPOINT = 'http://ec2-52-90-1-68.compute-1.amazonaws.com:8000/v1/';
 
 function FaviosMomHelper() {
 }
+
+FaviosMomHelper.prototype.requestOpenForBusiness = function(req,res) {
+
+};
 /*
 invoked by Alexa to retrieve menu
  */
@@ -82,15 +91,18 @@ FaviosMomHelper.prototype.requestMenuItem = function(menuItem) {
     );
 };
 /**
- * invoke the menuitems api
+ * invoke the menuitems api. This api is intended to get a list of items
  */
 FaviosMomHelper.prototype.getMenuItems = function(menuItem) {
+
+    var path = "menuitems/";
+    if (menuItem) path += menuItem;
 
     var options = {
 
         method: 'GET',
 
-        uri: ENDPOINT + "menuitems" + "/" + menuItem,
+        uri: ENDPOINT + path,
 
         resolveWithFullResponse: true,
 
@@ -100,7 +112,9 @@ FaviosMomHelper.prototype.getMenuItems = function(menuItem) {
 
     return rp(options);
 };
-
+/**
+ * Intended to do a lookup of a specific , at most one MenuItem
+ */
 FaviosMomHelper.prototype.getMenuItem = function(menuItem) {
 
     var options = {
@@ -211,36 +225,83 @@ FaviosMomHelper.prototype.formatProcessOrder = function(order) {
 
 FaviosMomHelper.prototype.addToCart = function(req,res) {
       var slotMenuItem, slotPortions, menuItem, total_order_price = 0;
+      var updated_order = [];
+      var menuitem_id;
+
       slotMenuItem = req.slot('MENUITEM');
       slotPortions = (req.slot('PORTIONS') ? req.slot('PORTIONS') : 1);
-      console.log("in add to Cart");
-      console.log("value of slotMenItem = " + slotMenuItem);
 
       return this.getMenuItem(slotMenuItem).then( function(response) {
           var current_order = (res.session('order') ? res.session('order') : []);
-          console.log(JSON.stringify(current_order));
-           console.log(JSON.stringify(response.body));
            if (response.body['statuscode'] == "200" && response.body['rows'] == 1) {
                menuItem = response.body['result'];
                menuItem[0].portions = slotPortions;
-               //calculate total order price
-               current_order.push(menuItem[0]);
+               //calculate total order price - also merge duplicate entries for the same menu_item
+               var t = merge_order(current_order,menuItem[0]);
+               if (t)
+                 current_order.push(menuItem[0]);
                for (var i =0; i <current_order.length;i++) {
+                   menuitem_id = current_order[i].id;
+                   filter(updated_order, menuitem_id);
                    total_order_price += current_order[i].portions * current_order[i].price;
                }
                current_order.total_order_price = total_order_price;
-              // current_order = JSON.parse(JSON.stringify(current_order));
                res.session('order', current_order);
                res.session('total_order_price', total_order_price);
-
+               return menuItem[0];
            }
+           return menuItem[0];
       });
 }
 
-FaviosMomHelper.prototype.formatAddToCart = function() {
-    var response;
+FaviosMomHelper.prototype.formatAddToCart = function(add_to_cart_item) {
 
-    response = "Add item to cart - TEST";
+
+    if (add_to_cart_item) {
+        var portions = parseInt(add_to_cart_item.portions);
+        response.prompt = "Added " + ((portions > 1) ? portions + " portions":" one portion") + " of " + add_to_cart_item.name + " to cart.";
+        response.reprompt = "Complete order and proceed to payment or keep on adding to the cart";
+    }
     return response;
 }
+
+FaviosMomHelper.prototype.confirmCart = function(req,res) {
+     return req.session('order') ? req.session('order') : [];
+}
+
+FaviosMomHelper.prototype.formatConfirmCart = function(order) {
+    var current_order_price = 0 , menuitem_price;
+    var response = { "prompt": "",
+        "reprompt": ""};
+
+    if (!order || !order.length) {
+        response.prompt = "No items in your cart";
+    } else {
+        for (var i=0; i<order.length;i++) {
+            menuitem_price = parseInt(order[i].price);
+            response.prompt +=  (i ? "," : "") + order[i].portions + " Portions of " + order[i].name;
+            current_order_price += menuitem_price * order[i].portions;
+        }
+        if (response.prompt) response.prompt = "You have " + response.prompt + " at total price of " + current_order_price;
+    }
+    return response;
+}
+function merge_order(array, menuItem) {
+    for (var i=0; i<array.length;i++) {
+        if (menuItem.id != array[i].id) continue;
+        array[i].portions = parseInt(array[i].portions) + parseInt(menuItem.portions );
+        return;
+    }
+    return menuItem;
+}
+
+function filter(array,value) {
+     for (var i=0; i<array.length;i++) {
+        if (array[i].id == value) return array[i];
+     }
+ return undefined;
+}
+
+
+
 module.exports = FaviosMomHelper;
--- index.js
+++ index.js
@@ -15,11 +15,13 @@
  */
     app.pre = function(req,res,type) {
         console.log("In Pre App - calling type " + type);
-        if (req.sessionDetails.application.applicationId!="amzn1.echo-sdk-ams.app.000000-d0ed-0000-ad00-000000d00ebe") {
+        //if (req.sessionDetails.application.applicationId!="amzn1.echo-sdk-ams.app.000000-d0ed-0000-ad00-000000d00ebe") {
+        //if (req.sessionDetails.application.applicationId!="arn:aws:lambda:us-east-1:318466891062:function:FaviosMom_Nodejs") {
+        //if (req.sessionDetails.application.applicationId!="arn:aws:lambda:us-east-1:318466891062:function:FaviosMom_Nodejs") {
             // Fail ungracefully
-            console.log("calling Fail in Pre App");
-            res.fail("Invalid applicationId");
-        }
+       //     console.log("calling Fail in Pre App");
+        //    res.fail("Invalid applicationId");
+      //  }
     };
 
     app.post = function(req,res,type,exception) {
@@ -36,6 +38,11 @@
 
 	  var prompt = "Welcome to Favios's Mom, Ask me to List Menu, Order Items.";
         res.session('customer', 'julio');
+        res.card({
+            type:    "Simple",
+            title:   "My Cool Card",  //this is not required for type Simple
+            content: "This is the\ncontent of my card"
+        });
       /*  res.session('order', {"order": [
             { "name": "Black Beans",
                 "menu_id": 456,
@@ -58,12 +65,24 @@
 
     app.dictionary = {"menuItems":["Pot of Black Beans","green","blue"]};
 
+    app.intent('open-for-business',
+        {
+            'slots': {
+                "DAY" : "AMAZON.DATE",
+            },
+            'utterances': [ "{are you|} open {|today|tomorrow|this weekend|} {-|DAY}"]
+        }, function(req,res) {
+             var faviosMomHelper = new FaviosMomHelper();
+
+             return false;
+        });
+
     app.intent('menu', {
 
         'slots': {
-            'TYPE': 'LITERAL'
+            'TYPE':'MENU_TYPE',
          },
-        'utterances': ['{|tell me|what is} menu {vegetarian|ham|pork|TYPE}']
+        'utterances': ['{tell me|what is|} {-|TYPE} menu' ]
     },
         function(req, res) {
             console.log("In menu Intent");
@@ -118,11 +137,19 @@
      app.intent('commit_order', {
     'slots': {
      },
-    "utterances":[ "{place|commit|} {-|Order}" ]
+    "utterances":[ "{place|commit} {|order}" ]
      },
      function(req,res) {
          console.log("In commit Order");
+         var prompt;
          var faviosMomHelper = new FaviosMomHelper();
+
+         // 1st make sure there are items in the cart
+         if (!req.session('order')) {
+             prompt = "No items in your cart to order";
+             res.say(prompt).reprompt("What would you like to do?").shouldEndSession(false).send();
+             return false;
+         }
          faviosMomHelper.processOrder(req, res).then( function (order) {
 
              res.clearSession('order');
@@ -130,29 +157,49 @@
 
              res.say(faviosMomHelper.formatProcessOrder(order)).shouldEndSession(false).send();
          }).catch(function (err) {
-             var prompt = 'Error processing Order';
+             prompt = 'Error processing Order';
              res.say(prompt).reprompt("What would you like to do?").shouldEndSession(false).send();
 
          });
          return false;
      });
 
+/**
+ * An intent that orally confirms the customers order which is represented by what is in the cart.
+ * This method will start to use the "response" as a json object that contains both the "prompt" and the "reprompt"
+ */
+    app.intent('confirm_cart', {
+        "slots": {
+
+         },
+        "utterances": ["{|tell me|what is|what's} {|my|the} {order|cart}" ]
+         },
+         function(req,res) {
+             var faviosMomHelper = new FaviosMomHelper();
+             var order = faviosMomHelper.confirmCart(req,res);
+             var response = faviosMomHelper.formatConfirmCart(order);
+             res.say(response.prompt).reprompt(response.reprompt).shouldEndSession(false).send();
+
+             return false;
+         });
+
 
 
     app.intent('add_to_cart', {
         'slots': {
 
-            'MENUITEM': 'MENUITEMS',
-            'PORTIONS':'NUMBER'
+            "MENUITEM": "MENUITEMS",
+            "PORTIONS": "AMAZON.NUMBER"
 
         },
-        "utterances":[ "{add|order|} {1-100|PORTIONS} {-|MENUITEM}" ]
+        "utterances":[ "{add|order|} {-|PORTIONS} {|of} {-|MENUITEM}" ]
     },
     function (req,res) {
         var faviosMomHelper = new FaviosMomHelper();
 
         faviosMomHelper.addToCart(req,res).then( function(response) {
-            res.say(faviosMomHelper.formatAddToCart()).shouldEndSession(false).send();
+            var reprompt = "Would you like to add more items to cart?"
+            res.say(faviosMomHelper.formatAddToCart(response)).reprompt(reprompt).shouldEndSession(false).send();
         }).catch(function(err) {
             var prompt = 'Error occurred in Adding to Cart';
             res.say(prompt).reprompt(prompt).shouldEndSession(false).send();
@@ -161,61 +208,11 @@
        // res.say("Item added to cart").shouldEndSession(false).send();
        return false;
     });
-/*
-       app.intent('airportinfo', {
 
-	 'slots': {
+//hack to support custom utterances in utterance expansion string
+var utterancesMethod = app.utterances;
+app.utterances = function() {
+    return utterancesMethod().replace(/\{\-\|/g, '{');
+};
 
-	    'AIRPORTCODE': 'FAACODES'
-
-	  },
-
-	  'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
-
-	},
-
-	  function(req, res) {
-		  //get the slot
-
-		  var airportCode = req.slot('AIRPORTCODE');
-
-		  var reprompt = 'Tell me an airport code to get delay information.';
-
-		  if (_.isEmpty(airportCode)) {
-
-			  var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
-
-			  res.say(prompt).reprompt(reprompt).shouldEndSession(false);
-
-			  return true;
-
-		  } else {
-
-			  var faaHelper = new FAADataHelper();
-
-			  faaHelper.requestAirportStatus(airportCode).then(function(airportStatus) {
-
-				  console.log(airportStatus);
-
-				  res.say(faaHelper.formatAirportStatus(airportStatus)).send();
-
-			  }).catch(function(err) {
-
-				  console.log(err.statusCode);
-
-				  var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
-
-				  res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
-
-			  });
-
-			  return false;
-
-		  }
-
-
-	  }
-
-	);
-*/
-	module.exports = app;
+module.exports = app;
